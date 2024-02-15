88484e8b850d16d788bcd285b74b50ea13782101
nurmuhammad_mamurjonov <nurmuhammad_mamurjonov@student.itpu.uz>
2024-02-15 07:49:12 +0500

    make http requests

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,206 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="text-red-500">{{ error }}</p>
+    <form
+      class="flex flex-col w-full items-center"
+      @submit.prevent="
+        !isUpdating
+          ? request('POST')
+          : request('PUT', JSON.stringify(formValues), formValues.id)
+      "
+    >
+      <h2 class="text-xl">Create a new product...</h2>
+      <label for="name" class="mt-3 mb-1 flex items-center justify-between">
+        Name:
+        <input
+          required
+          id="name"
+          type="text"
+          placeholder="Type a name..."
+          v-model.trim.lazy="formValues.name_uz"
+          class="border rounded px-3 py-1 ml-2"
+        />
+      </label>
+      <label
+        for="productType"
+        class="mt-3 mb-1 flex items-center justify-between"
+      >
+        Product Type:
+        <select
+          id="productType"
+          v-model="formValues.product_type_id"
+          required
+          class="rounded border px-3 py-1 ml-2"
+          value="0"
+        >
+          <option :value="formValues.product_type_id" disabled>
+            Select product id
+          </option>
+          <option value="1">yovvoyi hayvonlar</option>
+          <option value="2">daraxtlar</option>
+          <option value="3">o'simliklar</option>
+        </select>
+      </label>
+      <label for="cost" class="mt-3 mb-1 flex items-center justify-between">
+        Cost:
+        <input
+          required
+          id="cost"
+          type="text"
+          placeholder="Enter cost"
+          v-model.trim.lazy.number="formValues.cost"
+          class="border rounded px-3 py-1 ml-2 flex items-center justify-between"
+        />
+      </label>
+      <label for="address" class="mt-3 mb-1 flex items-center justify-between">
+        Address:
+        <input
+          required
+          id="address"
+          type="text"
+          placeholder="Enter an address..."
+          v-model.trim.lazy="formValues.address"
+          class="border rounded px-3 py-1 ml-2 flex items-center justify-between"
+        />
+      </label>
+      <button
+        v-if="!isUpdating"
+        type="submit"
+        class="mt-4 bg-green-500 px-3 py-1.5 rounded-xl text-white"
+      >
+        Submit
+      </button>
+      <button
+        v-if="isUpdating"
+        class="mt-4 bg-green-500 px-3 py-1.5 rounded-xl text-white"
+      >
+        Uptade
+      </button>
+    </form>
+
+    <div v-for="product in products" :key="product.id" class="card">
+      <div class="flex justify-between">
+        <strong class="text-xl">Name: {{ product.name_uz }}</strong>
+        <div>Product Type ID: {{ product.product_type_id }}</div>
+      </div>
+      <p>ID: {{ product.id }}</p>
+      <p>Cost: {{ product.cost }}</p>
+      <address>Address: {{ product.address }}</address>
+      <div class="flex items-center justify-between">
+        <time datetime="">Created Date: {{ product.created_date }}</time>
+        <div class="w-40 flex justify-around">
+          <button
+            class="p-2 rounded-md bg-orange-400 text-white"
+            @click="
+              formValues = product;
+              isUpdating = true;
+            "
+          >
+            Edit
+          </button>
+          <button class="danger" @click="request('DELETE', null, product.id)">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://94.158.54.194:9092",
+});
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      isUpdating: false,
+      error: "",
+      products: [],
+      formValues: {
+        name_uz: "",
+        product_type_id: null,
+        cost: null,
+        address: "",
+        created_date: Date.now(),
+      },
+    };
+  },
+  methods: {
+    async request(method, body, id) {
+      try {
+        let res;
+        switch (method) {
+          case "GET":
+            res = await api.get("/api/product/");
+            this.products = res.data;
+            break;
+          case "POST":
+            res = await api.post("/api/product/", this.formValues);
+            this.products.push(this.formValues);
+            break;
+          case "DELETE":
+            res = await api.delete(`/api/product/${id}`);
+            this.products = this.products.filter(
+              (product) => product.id !== id
+            );
+            break;
+          case "PUT":
+            res = await api.put(`/api/products/${id}`, JSON.stringify(body));
+            console.log(res);
+            break;
+          default:
+            console.error("Invalid request method:", method);
+        }
+      } catch (error) {
+        this.error = error;
+        console.error("Something went wrong with the request:", error);
+      }
+    },
+  },
+
+  mounted() {
+    this.request("GET");
+  },
+};
 </script>
 
 <style>
+@import url(./style.css);
+body {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background: #d0cba7;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 60%;
+}
+
+form input,
+select {
+  width: 60%;
+}
+.card {
+  width: 100%;
+  border: 3px solid rgb(76, 37, 37);
+  border-radius: 15px;
+  padding: 20px;
+  margin-top: 20px;
+}
+
+.danger {
+  background-color: rgb(184, 3, 3);
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 7px;
+  cursor: pointer;
 }
 </style>
